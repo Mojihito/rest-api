0127834140182077798f5ccd385b8af2a70f2e1b
Rafał Lorenz <vardius@gmail.com>
2016-06-07 13:49:23 +0200

    Add phpmyadmin

--- bin/deploy.sh
+++ bin/deploy.sh
@@ -4,9 +4,3 @@ composer install
 
 docker-composer build
 docker-composer up -d
-
-php bin/console doctrine:database:drop --force
-php bin/console doctrine:database:create
-php bin/console doctrine:schema:update --force
-
-php bin/console doctrine:fixtures:load
