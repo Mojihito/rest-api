0ec94a4e2b81b14dad5fc25cc545824a9d1187db
Rafał Lorenz <vardius@gmail.com>
2016-06-07 10:10:34 +0200

    Update docker config

--- docker/configure-mysql.sh
+++
@@ -1,3 +0,0 @@
-/usr/sbin/mysqld &
-sleep 5
-echo "GRANT ALL ON *.* TO root@'localhost' IDENTIFIED BY 'root' WITH GRANT OPTION; FLUSH PRIVILEGES" | mysql
--- docker/deploy.sh
+++
@@ -1,4 +0,0 @@
-/usr/sbin/mysqld &
-sleep 5
-cd /server
-/server/docker/commands.sh
