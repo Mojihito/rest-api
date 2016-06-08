187051de6cc6d7bd7feb7c626f2d834a56c70e17
Rafał Lorenz <vardius@gmail.com>
2016-06-08 08:39:53 +0200

    Add deploy script exec only fter mysql container ready

--- docker/php-fpm/deploy.sh
+++ docker/php-fpm/deploy.sh
@@ -1,9 +1,25 @@
 #!/usr/bin/env bash
 
+echo "Starting deploy"
+
+# Wait for the mysql container to be ready before deploying.
+echo "Stalling for Mysql"
+while true; do
+    nc -q 1 mysql 3306>/dev/null && break
+done
+
+echo "Installing vendors"
+
 composer install
+
+echo "Setting up database"
 
 php bin/console doctrine:database:drop --force
 php bin/console doctrine:database:create
 php bin/console doctrine:schema:update --force
 
+echo "Loading fixtures"
+
 php bin/console doctrine:fixtures:load
+
+echo "Done deploy"
