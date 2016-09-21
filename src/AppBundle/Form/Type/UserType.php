6700380dbd29eadf74b909496c03bdc41687db60
Rafał Lorenz <vardius@gmail.com>
2016-09-21 09:55:22 +0200

    Update UserType.php

--- src/AppBundle/Form/Type/UserType.php
+++ src/AppBundle/Form/Type/UserType.php
@@ -29,6 +29,8 @@ class UserType extends AbstractType
      */
     public function buildForm(FormBuilderInterface $builder, array $options)
     {
+        parent::buildForm($builder, $options);
+        
         $builder
             ->add('email', TextType::class, ['required' => false])
             ->add('name', TextType::class, ['required' => false])
