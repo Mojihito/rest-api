902182cd2d2344373a02fb3ae12359d933a00218
Rafał Lorenz <vardius@gmail.com>
2016-09-22 08:43:55 +0200

    Update deploy.sh

--- bin/deploy.sh
+++ bin/deploy.sh
@@ -1,16 +1,18 @@
 #!/usr/bin/env bash
 echo "Setting up database"
-php app/console doctrine:database:drop --force
-php app/console doctrine:database:create
+php bin/console doctrine:database:drop --force
+php bin/console doctrine:database:create
 
 echo "Create schema"
-php app/console doctrine:schema:update --force --em=default
+php bin/console doctrine:schema:update --force --em=default
 
 echo "Loading the default fixtures"
-php app/console doctrine:fixtures:load --em=default
+php bin/console doctrine:fixtures:load --em=default
 
 echo "Create user"
-php app/console fos:user:create test test@example.com password
-php app/console fos:user:promote test ROLE_SUPER_ADMIN
+php bin/console fos:user:create test test@example.com password --super-admin
+
+echo "Create oauth test client"
+php bin/console app:oauth:create test --grant-type="refresh_token" --grant-type="client_credentials" --grant-type="password"
 
 echo "Done deploy"
