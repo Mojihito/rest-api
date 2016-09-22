8edd0430e5b69ee5bbb5df17fa97baa9425658db
Rafał Lorenz <vardius@gmail.com>
2016-09-22 08:32:37 +0200

    Update deploy.sh

--- bin/deploy.sh
+++ bin/deploy.sh
@@ -1,6 +1,16 @@
-#! /bin/bash
+#!/usr/bin/env bash
+echo "Setting up database"
+php app/console doctrine:database:drop --force
+php app/console doctrine:database:create
 
-composer install
+echo "Create schema"
+php app/console doctrine:schema:update --force --em=default
 
-docker-composer build
-docker-composer up -d
+echo "Loading the default fixtures"
+php app/console doctrine:fixtures:load --em=default
+
+echo "Create user"
+php app/console fos:user:create test test@example.com password
+php app/console fos:user:promote test ROLE_SUPER_ADMIN
+
+echo "Done deploy"
